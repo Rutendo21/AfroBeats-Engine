15e979076f0107453d1627303e5b3b34c846c193
Rutendo21 <rutendomureriwa@gmail.com>
2023-03-16 16:32:02 +0200

    Commit 4

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 from flask import Flask, render_template, request
+import csv
 
 app = Flask(__name__)
 
 @app.route("/")
 def homepage():
     
-    return render_template("index.html")
+    if request.method == "GET":
+        Artists = []
+        file = open("AfroBeatsArtists.csv", "r")
+        reader = csv.reader(file)
+        next(reader)
+        for row in reader:
+            Artist = row
+            Artists.append(Artist)
+        
+        return render_template("index.html", Artists=Artists)
 
 if __name__ == "__main__":
     app.run()
